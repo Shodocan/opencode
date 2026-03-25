b79460114268557be087d3cd6855b40ba20b2990
Kit Langton <kit.langton@gmail.com>
2026-03-25 14:44:06 -0400

    rename instance context shape

--- packages/opencode/src/effect/instance-context.ts
+++ packages/opencode/src/effect/instance-context.ts
@@ -2,13 +2,13 @@ import { ServiceMap } from "effect"
 import type { Project } from "@/project/project"
 
 export declare namespace InstanceContext {
-  export interface Shape {
+  export interface Info {
     readonly directory: string
     readonly worktree: string
     readonly project: Project.Info
   }
 }
 
-export class InstanceContext extends ServiceMap.Service<InstanceContext, InstanceContext.Shape>()(
+export class InstanceContext extends ServiceMap.Service<InstanceContext, InstanceContext.Info>()(
   "opencode/InstanceContext",
 ) {}
